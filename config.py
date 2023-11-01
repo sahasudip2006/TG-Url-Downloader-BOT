b220e70e995c9f86dad42eb4a17955076574db80
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2023-11-01 19:01:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,49 @@
+import os
 
+class Config(object):
+  API_ID = int(os.environ.get("API_ID", "29849415"))
+  API_HASH = os.environ.get("API_HASH", "0dd6c10897b85d7f10a8dcdeb74f8b8a")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6554190225:AAE3MDqF3FXrKQWHbb_TZdnaZO2d9h24nfg")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "PRO_FILE_SAVE_BOT")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001763943446"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "TinyFy.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "f999bc3f142a37b8db08754279b7be7a81ef8604")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5165943027"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dahif89943:sudipsaha2006@cluster0.iscxsme.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001953586887")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001565949579"))
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  ABOUT_BOT_TEXT = f"""
+This is a Permanent FileStore Bot. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
+│
+├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+│
+├🔸 Language: [Python 3](https://www.python.org)
+│
+├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+  ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [SUDIP](https://telegram.me/SUDIPSAHA06)
+ 
+ I am not professional bot developer Please Support My Hard Work.
+
+[Donate Me](https://t.me/SUDIPSAHA06)
+"""
+  HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+
+How to Use Bot & it's Benefits??
+
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+"""
